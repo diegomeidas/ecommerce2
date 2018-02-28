f93b27ccd0252155f5505ef217f3c841a0cd844b
diegomeidas <diegomeidas@hotmail.com>
2018-02-28 19:22:41 -0300

    criando PageAdmin

--- index.php
+++ index.php
@@ -2,22 +2,33 @@
 
 require_once("vendor/autoload.php");
 
+
 use Slim\Slim;
 use Hcode\Page;
+use Hcode\PageAdmin;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
 
+    //ROTA INDEX DO SITE
+    $app->get('/', function() {
 
-$app->get('/', function() {
+        $page = new Page();         //nesse momento chama o 'header.html'
+        $page->setTpl("index");     //chama o index.html
+        //ao final chama o destruct 'footer.html'
+    });
 
-    $page = new Page();         //nesse momento chama o 'header.html'
-    $page->setTpl("index");     //chama o index.html
-                                //ao final chama o destruct 'footer.html'
-});
 
-$app->run();
 
- ?>
+    //ROTA DO ADMIN
+    $app->get('/admin', function() {
+
+        $page = new PageAdmin();
+        $page->setTpl("index");
+    });
+
+    $app->run();
+
+?>
